2d19f437c3aa092864d608ec775eb2679feea4d9
jakubzemla <jakubzemla@gmail.com>
2023-01-07 20:59:16 +0100

    Setting up and uploading the projectt

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+// function changeLogoColor() {
+//     if (logoWhite == true) {
+//         navbarLogo.src = "./images/header_logo3.png"
+//         logoWhite = false
+//     }
+//     else {
+//         navbarLogo.src = "./images/header_logo3orange.png"
+//         logoWhite = true
+//     }
+// }
+
+// let logoWhite = true
+// let navbarLogo = document.querySelector("#navbar-logo")
+// navbarLogo.addEventListener("mouseover", changeLogoColor)
+// navbarLogo.addEventListener("mouseout", changeLogoColor)

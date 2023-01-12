768495396c8053323e4fd1348822bb47183c1013
jakubzemla <jakubzemla@gmail.com>
2023-01-12 14:31:19 +0100

    Add course section with jQuery carousel

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,4 @@
-// function changeLogoColor() {
-//     if (logoWhite == true) {
-//         navbarLogo.src = "./images/header_logo3.png"
-//         logoWhite = false
-//     }
-//     else {
-//         navbarLogo.src = "./images/header_logo3orange.png"
-//         logoWhite = true
-//     }
-// }
-
-// let logoWhite = true
-// let navbarLogo = document.querySelector("#navbar-logo")
-// navbarLogo.addEventListener("mouseover", changeLogoColor)
-// navbarLogo.addEventListener("mouseout", changeLogoColor)
+$(document).ready(function() {
+	$('.carousel').carousel();
+});
+ 
